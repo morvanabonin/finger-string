d160ba574b6bfee4921fa928d84c6c5f695a9abe
Morvana Bonin <morvana.bonin@kinghost.com.br>
2017-07-05 14:30:08 -0300

    Acréscimo da config de banco de dados.

--- configs/settings.php
+++ configs/settings.php
@@ -1,1 +1,11 @@
 <?php
+return [
+    'settings' => [
+        'db' => [
+            'host' => '172.19.0.2',
+            'user' => 'fingerstring',
+            'pass' => 'f1ng3r',
+            'dbname' => 'fingerstring'
+        ]
+    ],
+];
