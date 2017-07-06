99dde7a4c2b9a65be8338632d80de053d733558b
Morvana Bonin <morvana.bonin@kinghost.com.br>
2017-07-06 18:34:15 -0300

    More things :P

---
+++ configs/routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morvana
+ * Date: 06/07/17
+ * Time: 18:30
+ */
--- public/index.php
+++ public/index.php
@@ -1,8 +1,30 @@
 <?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__.$url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
-// Create and configure Slim app
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
 
+// Instantiate the app
+$settings = require __DIR__ . '/../configs/settings.php';
+$app = new \Slim\App($settings);
 
+// Set up dependencies
+require __DIR__ . '/../configs/dependencies.php';
 
+// Register middleware
+require __DIR__ . '/../configs/middleware.php';
+
+// Register routes
+require __DIR__ . '/../configs/routes.php';
+
+// Run app
+$app->run();
